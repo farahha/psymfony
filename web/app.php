fdcb5523a1ff0df08893e62e1321e14847280739
Sofiane SADOUD <sofiane@MacBook-Pro-de-Sofiane.local>
2017-03-22 18:35:55 +0100

    Ajout de commentaires dans le controleur frontal app.php

--- web/app.php
+++ web/app.php
@@ -6,13 +6,13 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
+$kernel = new AppKernel('prod', false); // Appel du noyau Kernel
+$kernel->loadClassCache(); // On charge les classes présentes dans le cache pour gagner en temps
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+$request = Request::createFromGlobals(); // Récuperation de la requête
+$response = $kernel->handle($request); // Exécution de la requête et récupération de la réponse
+$response->send(); // Envoi de la réponse à la requête demandée
+$kernel->terminate($request, $response); // fin du cycle requête/reponse
