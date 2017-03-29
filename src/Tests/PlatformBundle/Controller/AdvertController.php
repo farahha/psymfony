f94db60e3819f42288d81ca827653cfaaf0737b0
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-03-29 14:34:40 +0200

    Préparation corps du 'controller' et des 'templates'

--- src/Tests/PlatformBundle/Controller/AdvertController.php
+++ src/Tests/PlatformBundle/Controller/AdvertController.php
@@ -4,7 +4,6 @@
 
 namespace Tests\PlatformBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -13,34 +12,53 @@ class AdvertController extends Controller
 {
     public function indexAction($page)
     {
-        if ($page < 1){
-            return new NotFoundHttpException('Page N° ' . $page . ' introuvable !');
+        if (empty($page) || $page < 1){
+            throw new NotFoundHttpException('Page N° "' . $page . '" introuvable !');
         }
-        // Appelle du template
-        return $this->render('TestsPlatformBundle:Advert:index.html.twig');
+        return $this->render('TestsPlatformBundle:Advert:index.html.twig', ['page' => $page]);
     }
 
     public function addAction(Request $request)
     {
         if ($request->isMethod('POST')){
+            // Code pour traiter la requête ...
+            $this->addFlash('info', 'Annonce bien enregistrée');
             return $this->redirectToRoute('tests_platform_view', ['id' => 5]); // le 5 c'est juste pour le test
         }
 
         return $this->render('TestsPlatformBundle:Advert:add.html.twig');
     }
 
-    public function editAction($id)
+    public function editAction($id, Request $request)
     {
-        return new Response("Modifier ? Quoi ???? ... bah l'annonce dont l'id est : ". $id);
+        if ($request->isMethod('POST')){
+            // Code pour traiter la requête ...
+            $this->addFlash('Info', 'Annonce '.$id.' bien modifiée');
+            return $this->redirectToRoute('tests_platform_view', ['id' => $id]);
+        }
+
+        return $this->render('TestsPlatformBundle:Advert:edit.html.twig');
     }
 
     public function deleteAction($id)
     {
-        return new Response("Supprimer ? Quoi ???? ... bah l'annonce dont l'id est : " . $id);
+        // Code pour traiter la requête ...
+        return $this->render('TestsPlatformBundle:Advert:delete.html.twig', ['id' => $id]);
     }
 
     public function viewAction($id)
     {
+        // Code pour traiter la requête ...
         return $this->render('TestsPlatformBundle:Advert:view.html.twig', ['id' => $id]);
+    }
+
+    // Fonction permettant de rajouter un message falsh
+    protected function addFlash($type, $message)
+    {
+        if (!$this->container->has('session')) {
+            throw new \LogicException('Impossible d\'utiliser les messages flash, le contaner n\'a pas démarré sa session.');
+        }
+
+        $this->container->get('session')->getFlashBag()->add($type, $message);
     }
 }
