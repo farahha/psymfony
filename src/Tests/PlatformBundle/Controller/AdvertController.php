e35757ac32f46c99f2ca9caf25705bc653cb8119
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-04-01 22:58:45 +0200

    Création des tous les templates basés sur le principe de l'héritage

--- src/Tests/PlatformBundle/Controller/AdvertController.php
+++ src/Tests/PlatformBundle/Controller/AdvertController.php
@@ -15,7 +15,33 @@ class AdvertController extends Controller
         if (empty($page) || $page < 1){
             throw new NotFoundHttpException('Page N° "' . $page . '" introuvable !');
         }
-        return $this->render('TestsPlatformBundle:Advert:index.html.twig', ['page' => $page]);
+
+        // Notre liste d'annonce en dur
+
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
+        //return $this->render('TestsPlatformBundle:Advert:index.html.twig', ['page' => $page]);
+        return $this->render('TestsPlatformBundle:Advert:index.html.twig', array(
+                'listAdverts' => $listAdverts
+        ));
     }
 
     public function addAction(Request $request)
@@ -37,19 +63,62 @@ class AdvertController extends Controller
             return $this->redirectToRoute('tests_platform_view', ['id' => $id]);
         }
 
-        return $this->render('TestsPlatformBundle:Advert:edit.html.twig');
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('TestsPlatformBundle:Advert:edit.html.twig', array(
+                'advert' => $advert
+        ));
     }
 
     public function deleteAction($id)
     {
-        // Code pour traiter la requête ...
-        return $this->render('TestsPlatformBundle:Advert:delete.html.twig', ['id' => $id]);
+
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+        return $this->render('TestsPlatformBundle:Advert:delete.html.twig', ['advert' => $advert]);
     }
 
     public function viewAction($id)
     {
-        // Code pour traiter la requête ...
-        return $this->render('TestsPlatformBundle:Advert:view.html.twig', ['id' => $id]);
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('TestsPlatformBundle:Advert:view.html.twig', array(
+            'advert' => $advert
+        ));
+    }
+
+    public function menuAction($limit)
+    {
+        // On fixe en dur une liste ici, bien entendu par la suite
+        // on la récupérera depuis la BDD !
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('TestsPlatformBundle:Advert:menu.html.twig', array(
+            // Tout l'intérêt est ici : le contrôleur passe
+            // les variables nécessaires au template !
+            'listAdverts' => $listAdverts
+        ));
     }
 
     // Fonction permettant de rajouter un message falsh
