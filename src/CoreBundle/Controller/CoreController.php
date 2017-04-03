ca6fcec0d5dcc7e1ad7ee9115e8d164a5d408e61
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-04-03 22:38:27 +0200

    CS fix

--- src/CoreBundle/Controller/CoreController.php
+++ src/CoreBundle/Controller/CoreController.php
@@ -12,23 +12,23 @@ class CoreController extends Controller
         // je vais mettre comme pour le reste de l'appli, les annonces à affichier dans la page d'accueil en dur :/
         $listAdverts = array(
             array(
-                    'title'   => 'Recherche développpeur Symfony',
-                    'id'      => 1,
-                    'author'  => 'Alexandre',
-                    'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
-                    'date'    => new \Datetime()),
+                'title'   => 'Recherche développpeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
             array(
-                    'title'   => 'Mission de webmaster',
-                    'id'      => 2,
-                    'author'  => 'Hugo',
-                    'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-                    'date'    => new \Datetime()),
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
             array(
-                    'title'   => 'Offre de stage webdesigner',
-                    'id'      => 3,
-                    'author'  => 'Mathieu',
-                    'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-                    'date'    => new \Datetime())
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
         );
 
         //return $this->render('TestsPlatformBundle:Advert:index.html.twig', ['page' => $page]);
