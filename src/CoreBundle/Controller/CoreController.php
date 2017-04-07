bbcd65f55f2a3f8a44fcb7d216fc98f00e509b8c
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-04-07 12:06:21 +0200

    MAJ Core controleur : Refactoring

--- src/CoreBundle/Controller/CoreController.php
+++ src/CoreBundle/Controller/CoreController.php
@@ -8,33 +8,7 @@ class CoreController extends Controller
 {
     public function indexAction()
     {
-        // Vue que pour le moment, je n'ai pas encore connecté mon appli à une BDD,
-        // je vais mettre comme pour le reste de l'appli, les annonces à affichier dans la page d'accueil en dur :/
-        $listAdverts = array(
-            array(
-                'title'   => 'Recherche développpeur Symfony',
-                'id'      => 1,
-                'author'  => 'Alexandre',
-                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
-                'date'    => new \Datetime()),
-            array(
-                'title'   => 'Mission de webmaster',
-                'id'      => 2,
-                'author'  => 'Hugo',
-                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
-                'date'    => new \Datetime()),
-            array(
-                'title'   => 'Offre de stage webdesigner',
-                'id'      => 3,
-                'author'  => 'Mathieu',
-                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
-                'date'    => new \Datetime())
-        );
-
-        //return $this->render('TestsPlatformBundle:Advert:index.html.twig', ['page' => $page]);
-        return $this->render('CoreBundle:Core:index.html.twig', array(
-                'listAdverts' => $listAdverts
-        ));
+        return $this->render('CoreBundle:Core:index.html.twig');
     }
 
     public function contactAction()
