893e44529a3a6c5873c5353af5c8b9b1a3f85a4a
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-05-18 13:46:10 +0200

    Création service listener + config de Swift mailer

---
+++ src/Tests/PlatformBundle/DoctrineListener/ApplicationCreationListener.php
@@ -0,0 +1,35 @@
+<?php
+namespace Tests\PlatformBundle\DoctrineListener;
+
+use Tests\PlatformBundle\Email\ApplicationMailer;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Tests\PlatformBundle\Entity\Application;
+
+class ApplicationCreationListener
+{
+    /**
+     *
+     * @var ApplicationMailer
+     */
+    private $applicationMailer;
+
+    public function __construct(ApplicationMailer $applicationMailer)
+    {
+        $this->applicationMailer = $applicationMailer;
+    }
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+
+        if (!$entity instanceof Application) {
+            return;
+        }
+
+        try {
+            $this->applicationMailer->sendEmail($entity);
+        } catch (\Exception $e){
+            // Next time
+        }
+    }
+}
