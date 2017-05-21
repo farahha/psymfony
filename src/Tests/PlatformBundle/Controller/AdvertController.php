8f4235e95880f6d07421c33caa3a777644def9ea
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-05-21 17:36:57 +0200

    Modifications : Préparation du controlleur AdvertController à la gestion des formulaires

--- src/Tests/PlatformBundle/Controller/AdvertController.php
+++ src/Tests/PlatformBundle/Controller/AdvertController.php
@@ -22,11 +22,10 @@ class AdvertController extends Controller
 
         $repository = $this->getDoctrine()->getManager()->getRepository('Tests\PlatformBundle\Entity\Advert');
         $listAdverts = $repository->getArrayAdverts(); // Plus rapide que findAll() mais ne prend pas en compte les modifications
-        $nbAdverts = $repository->getNbAdverts();
-        //return $this->render('TestsPlatformBundle:Advert:index.html.twig', ['page' => $page]);
+        $nbAdverts = $repository->getNbAdverts(); // Juste pour le test, car je peux faire count($listAdverts) directement
         return $this->render('TestsPlatformBundle:Advert:index.html.twig', array(
-                'listAdverts' => $listAdverts,
-                'nbAdverts' => (int) $nbAdverts,
+            'listAdverts' => $listAdverts,
+            'nbAdverts' => (int) $nbAdverts,
         ));
     }
 
@@ -41,69 +40,55 @@ class AdvertController extends Controller
             'http://www.business-agility.com/wp-content/uploads/2014/07/850x236-events.jpg',
             'http://www.karmatechnologies.asia/wp-content/uploads/2015/11/Web-Applications.jpg',
         ];
-
         $title = $speciality[round(rand(0, 3))];
 
-        // création d'une entité Advert
-        $advert = new Advert();
-        $advert->setTitle($title);
-        $advert->setAuthor('KabyliXX');
-        $advert->setContent('Nous recherchons pour notre entreprise spécialisée dans le développement un développeur '.$title);
-
-        // Création d'une image
-        $image = new Image();
-        $image->setUrl($urls[round(rand(0, 3))]);
-        $image->setAlt("Je n'ai pas trouvé d'image");
-
-        // Setter la prepriété de l'annonce avec l'objet Image
-        $advert->setImage($image);
-
-        // On va récupérer l'entité manager pour persister l'entité advert
-        $em = $this->getDoctrine()->getManager();
-
-        // Récupération du repo
-        $repository = $this->getDoctrine()->getRepository("Tests\PlatformBundle\Entity\Skill");
-
-        // récupération de tous les skills
-        $listSkills = $repository->findAll();
-
-        foreach ($listSkills as $skill) {
-            // Pour chaque skill, on crée une relation avec l'annonce
-            $advertSkill = new AdvertSkill();
-            // On lie la relation avec l'annonce
-            $advertSkill->setAdvert($advert);
-            // on lie la compétence avec l'annonce
-            $advertSkill->setSkill($skill);
-            // On lui attribue un niveau
-            $advertSkill->setLevel($level[round(rand(0, 2))]); // Oui c'est pas joli :/
-            // On persiste la relation
-            $em->persist($advertSkill);
-        }
-
-        // 1 - Persister l'entité (<=> preparation de la requete)
-        // A noter aussi : Persist fonctionne comme une transaction dans laquelle on met differentes requetes a executer
-        $em->persist($advert);
-
-        // 2 - flusher tout ce qui a été persisté (<=> a l'execution des requetes preparées INSERT, UPDATE, DELETE ...)
-        // Le flush comme un commit, valide les requetes contenues dans le persist et si y a pas d'erreur c'est OK sinon rollback
-        $em->flush();
-
         if ($request->isMethod('POST')) {
-            // Code pour traiter la requête ...
-            $this->addFlash('info', 'Annonce bien enregistrée');
-            return $this->redirectToRoute('tests_platform_view', ['id' => 5]); // le 5 c'est juste pour le test
-        }
-/*
-        $antispam= $this->container->get('tests_platform.antispam');
-        $txt = 'un deux ...';
-        if ($antispam->isSpam($txt))
-        {
-            throw new \Exception("Vous annonce a été détecté comme spam ...");
-        }
-*/
-        return $this->redirectToRoute('tests_platform_view', array('id' => $advert->getId()));
+            $antispam = $this->container->get('tests_platform.antispam');
 
-        //return $this->render('TestsPlatformBundle:Advert:add.html.twig');
+            // Récupération des valeur envoyées dans le formulaire.
+            $title = htmlspecialchars($request->get('title', $title)); // Le second parmètre est par défaut (pas clean)
+            $author = htmlspecialchars($request->get('author', 'kabyliXX'));
+            $content = htmlspecialchars($request->get('content', 'Nous recherchons pour notre entreprise spécialisée dans le développement un développeur '.$title));
+
+            // création d'une entité Advert
+            $advert = new Advert();
+            $advert->setTitle($title);
+            $advert->setAuthor($author);
+            $advert->setContent($content);
+
+            if ($antispam->isSpam($content)) {
+                $advert->setPublished(false);
+                //throw new \Exception("Vous annonce a été détecté comme spam ...");
+            }
+
+            // Création d'une image
+            $image = new Image();
+            $image->setUrl($urls[round(rand(0, 3))]);
+            $image->setAlt("Je n'ai pas trouvé d'image");
+            $advert->setImage($image);
+
+            $em = $this->getDoctrine()->getManager();
+            $repository = $this->getDoctrine()->getRepository("Tests\PlatformBundle\Entity\Skill");
+
+            // récupération de tous les skills
+            $listSkills = $repository->findAll();
+            foreach ($listSkills as $skill) {
+                $advertSkill = new AdvertSkill();
+                $advertSkill->setAdvert($advert);
+                $advertSkill->setSkill($skill);
+                $advertSkill->setLevel($level[round(rand(0, 2))]); // Oui c'est pas joli :/
+                $em->persist($advertSkill);
+            }
+
+            $em->persist($advert);
+            $em->flush();
+
+            $this->addFlash('info', 'Votre annonce ('.$advert->getTitle().') a bien été enregistrée.');
+            return $this->redirectToRoute('tests_platform_view', ['id' => $advert->getId()]); // le 5 c'est juste pour le test
+        }
+
+        $this->addFlash('info', 'Le formulaire d\'ajout d\'annonce n\'est pas encore créé.');
+        return $this->render('TestsPlatformBundle:Advert:add.html.twig');
     }
 
     public function editAction($id, Request $request)
@@ -119,17 +104,15 @@ class AdvertController extends Controller
         if (null === $advert) {
             throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
         }
-
+/*
         $em = $this->getDoctrine()->getManager();
 
-        // La méthode findAll retourne toutes les catégories de la base de données
         $listCategories = $em->getRepository('Tests\PlatformBundle\Entity\Category')->findAll();
 
-        // On boucle sur les catégories pour les lier à l'annonce
         foreach ($listCategories as $category) {
             $advert->addCategory($category);
         }
-
+*/
         // Pour persister le changement dans la relation, il faut persister l'entité propriétaire
         // Ici, Advert est le propriétaire, donc inutile de la persister car on l'a récupérée depuis Doctrine
 
@@ -137,8 +120,46 @@ class AdvertController extends Controller
         $em->flush();
 
         if ($request->isMethod('POST')) {
-            // Code pour traiter la requête ...
-            $this->addFlash('Info', 'Annonce '.$id.' bien modifiée');
+            $antispam = $this->container->get('tests_platform.antispam');
+
+            // Récupération des valeur envoyées dans le formulaire.
+            if (!empty($request->get('title'))) {
+                $title = htmlspecialchars($request->get('title'));
+            }
+            if (!empty($request->get('author'))) {
+                $author = htmlspecialchars($request->get('author'));
+            }
+            if (!empty($request->get('content'))) {
+                $content = htmlspecialchars($request->get('content'));
+            }
+
+            $advert->setTitle($title);
+            $advert->setAuthor($author);
+            $advert->setContent($content);
+
+            if ($antispam->isSpam($content)) {
+                $advert->setPublished(false);
+                //throw new \Exception("Vous annonce a été détecté comme spam ...");
+            }
+
+            $image = new Image();
+            $image->setUrl($urls[round(rand(0, 3))]);
+            $image->setAlt("Je n'ai pas trouvé d'image");
+            $advert->setImage($image);
+
+            $repository = $this->getDoctrine()->getRepository("Tests\PlatformBundle\Entity\Skill");
+
+            // récupération de tous les skills
+            $listSkills = $repository->findAll();
+            foreach ($listSkills as $skill) {
+                $advertSkill = new AdvertSkill();
+                $advertSkill->setAdvert($advert);
+                $advertSkill->setSkill($skill);
+                $advertSkill->setLevel($level[round(rand(0, 2))]); // Oui c'est pas joli :/
+                $em->persist($advertSkill);
+            }
+
+            $this->addFlash('Info', 'L\'annonce '.$advert->getTitle().' a bien été mise à jour');
             return $this->redirectToRoute('tests_platform_view', ['id' => $id]);
         }
 
@@ -186,10 +207,12 @@ class AdvertController extends Controller
         }
 
         // On récupère la liste des candidatures
-        $applications = $this->getDoctrine()->getRepository("TestsPlatformBundle:Application")->findBy(['advert' => $advert]);
+        $applications = $this->getDoctrine()->getRepository("TestsPlatformBundle:Application")
+                        ->findBy(['advert' => $advert]);
 
         // On récupère les compétences
-        $advertSkills = $this->getDoctrine()->getRepository("TestsPlatformBundle:AdvertSkill")->findBy(['advert' => $advert]);
+        $advertSkills = $this->getDoctrine()->getRepository("TestsPlatformBundle:AdvertSkill")
+                        ->findBy(['advert' => $advert]);
 
         return $this->render('TestsPlatformBundle:Advert:view.html.twig', array(
             'advert' => $advert,
