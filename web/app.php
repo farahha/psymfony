01cecce1e7a452f93d3c6f1bcc7bf6399ea10452
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-05-22 13:56:52 +0200

    Remove comments

--- web/app.php
+++ web/app.php
@@ -8,10 +8,8 @@ include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false); // Appel du noyau Kernel
 $kernel->loadClassCache(); // On charge les classes présentes dans le cache pour gagner en temps
-//$kernel = new AppCache($kernel);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter : Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals(); // Récuperation de la requête
 $response = $kernel->handle($request); // Exécution de la requête et récupération de la réponse
 $response->send(); // Envoi de la réponse à la requête demandée
