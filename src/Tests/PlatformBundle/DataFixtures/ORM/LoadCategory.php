e1fba50d4d91edfe1e9a3918d3599d63cf4a6919
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-05-25 20:43:19 +0200

    CS Fix Data Fixtures

--- src/Tests/PlatformBundle/DataFixtures/ORM/LoadCategory.php
+++ src/Tests/PlatformBundle/DataFixtures/ORM/LoadCategory.php
@@ -10,27 +10,27 @@ use Tests\PlatformBundle\Entity\Category;
 class LoadCategory implements FixtureInterface
 {
     // Dans l'argument de la méthode load, l'objet $manager est l'EntityManager
-  public function load(ObjectManager $manager)
-  {
-      // Liste des noms de catégorie à ajouter
-    $names = array(
-      'Développement web',
-      'Développement mobile',
-      'Graphisme',
-      'Intégration',
-      'Réseau'
-    );
+    public function load(ObjectManager $manager)
+    {
+        // Liste des noms de catégorie à ajouter
+        $names = array(
+            'Développement web',
+            'Développement mobile',
+            'Graphisme',
+            'Intégration',
+            'Réseau'
+        );
 
-      foreach ($names as $name) {
-          // On crée la catégorie
-      $category = new Category();
-          $category->setName($name);
+        foreach ($names as $name) {
+            // On crée la catégorie
+            $category = new Category();
+            $category->setName($name);
 
-      // On la persiste
-      $manager->persist($category);
-      }
+            // On la persiste
+            $manager->persist($category);
+        }
 
-    // On déclenche l'enregistrement de toutes les catégories
-    $manager->flush();
-  }
+        // On déclenche l'enregistrement de toutes les catégories
+        $manager->flush();
+    }
 }
