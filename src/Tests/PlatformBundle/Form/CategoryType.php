61c7ffb0143fa6b30fc7dbd1dcef9fb491add0ba
Sofiane SADOUD <sofiane.sadoud@gmail.com>
2017-06-07 17:09:43 +0200

    Suite formulaire annonce : mise en forme

--- src/Tests/PlatformBundle/Form/CategoryType.php
+++ src/Tests/PlatformBundle/Form/CategoryType.php
@@ -5,7 +5,7 @@ namespace Tests\PlatformBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategoryType extends AbstractType
 {
@@ -14,7 +14,7 @@ class CategoryType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', TextareaType::class);
+        $builder->add('name', TextType::class);
     }
 
     /**
