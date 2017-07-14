8be6814dae276177ce75d9e4b178ff041d671ca2
Sofiane SADOUD <sofiane.sadoud@hotmail.fr>
2017-07-15 00:16:35 +0200

    Recommandation : Suppression de la variable rootNode du DI/conf

--- src/Library/PlatformBundle/DependencyInjection/Configuration.php
+++ src/Library/PlatformBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('library_platform');
+        $treeBuilder->root('library_platform');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
